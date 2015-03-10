577f0a968f195fd59fe7271a99e495127254dca2
Tony Zheng <zheng.tony6@gmail.com>
2015-03-10 19:47:13 -0400

    Update CalculateScores.scala

--- CalculateScores.scala
+++ CalculateScores.scala
@@ -27,6 +27,7 @@ object CalculateScores
     val myTest = new Test(args(0))
     myTest.getK()
     myTest.run()
+    myTest.getTopK()
     //myTest.printAll()
     //myTest.printCategories()
     myTest.getStats()
